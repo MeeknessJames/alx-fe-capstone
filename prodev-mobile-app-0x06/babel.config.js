60d553362efb09e6a608df74499bc46647886d7f
Meekness James <meeknessjames1@gmail.com>
2025-09-11 13:02:17 +0100

    Corrected errors in 0x06 based on checker feedback

--- prodev-mobile-app-0x06/babel.config.js
+++ prodev-mobile-app-0x06/babel.config.js
@@ -1,10 +1,9 @@
 module.exports = function (api) {
   api.cache(true);
   return {
-    presets: ["babel-preset-expo"],
+    presets: ['babel-preset-expo'],
     plugins: [
-      "nativewind/babel",
-      "expo-router/babel",
+      require.resolve('expo-router/babel'),
     ],
   };
 };
