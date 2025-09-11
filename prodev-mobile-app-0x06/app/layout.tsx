18b1f559de1b72c44c48fed24c4c7ec5a95693e4
Meekness James <meeknessjames1@gmail.com>
2025-09-11 15:26:32 +0100

    fix: update app/layout.tsx with SafeAreaProvider and Tailwind import

--- prodev-mobile-app-0x06/app/layout.tsx
+++ prodev-mobile-app-0x06/app/layout.tsx
@@ -1,20 +1,17 @@
 import "../styles/global.css";
 import { Stack } from "expo-router";
 import { StatusBar } from "expo-status-bar";
+import { SafeAreaProvider } from "react-native-safe-area-context";
 
 export default function Layout() {
   return (
-    <>
+    <SafeAreaProvider>
       <Stack
         screenOptions={{
           headerShown: false,
         }}
-      >
-        <Stack.Screen name="index" />
-        <Stack.Screen name="join" />
-        <Stack.Screen name="signin" />
-      </Stack>
+      />
       <StatusBar style="auto" />
-    </>
+    </SafeAreaProvider>
   );
 }
